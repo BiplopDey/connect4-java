31dc9265b3040a1da234f0bb2bdbbdf41ff23569
Biplop <deybiplop4@gmail.com>
2025-08-25 16:15:23 +0200

    add sonar

--- src/main/java/com/minimax/minimax/connect4/service/Buggy.java
+++
@@ -1,8 +0,0 @@
-package com.minimax.minimax.connect4.service;
-
-public class Buggy {
-    public String upper(String s) {
-        return s.toUpperCase(); // posible NPE -> bug en muchas reglas
-    }
-}
-
